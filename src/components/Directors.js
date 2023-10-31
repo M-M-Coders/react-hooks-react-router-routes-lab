2145911448f18143c33c9b5ac0d6934f8310ae6c
anita-that-chiq <62019551+kahenya-anita@users.noreply.github.com>
2023-10-31 10:33:02 +0300

    Home, Directors, Navbar and App component

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,7 +2,17 @@ import React from "react";
 import { directors } from "../data";
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div>
+      <h1>Directors Page</h1>
+      {directors.map((director, index) => (
+        <ul key={index}>
+          <li>Name: {director.name}</li>
+          <li>Name: {director.movies}</li>
+        </ul>
+      ))}
+    </div>
+  );
 }
 
 export default Directors;
